cbb78f164a2467d879a9a0f5f291eb7237a2f5b7
Doc Shadrach <133315906+DocShadrach@users.noreply.github.com>
2024-03-12 22:50:33 +0100

    Add files via upload

---
+++ install_libraries.py
@@ -0,0 +1,44 @@
+import subprocess
+
+# List of libraries to install
+libraries_to_install = [
+    'ffmpeg-python',
+    'tqdm',
+    'scipy',
+    'soundfile',
+    'numpy',
+]
+
+for library in libraries_to_install:
+    try:
+        subprocess.check_call(['pip3', 'install', library])
+        print(f'{library} installed successfully')
+    except subprocess.CalledProcessError:
+        print(f'Error installing {library}. Continuing with the next library...')
+        continue
+
+# Check if tkinter is available
+try:
+    import tkinter
+    print('tkinter is already available.')
+except ImportError:
+    print('tkinter is not available. Installing...')
+    subprocess.check_call(['pip3', 'install', 'tkinter'])
+    print('tkinter installed successfully.')
+
+# Check if webbrowser and shutil are available
+try:
+    import webbrowser
+    print('webbrowser is already available.')
+except ImportError:
+    print('webbrowser is not available. Installing...')
+    subprocess.check_call(['pip3', 'install', 'webbrowser'])
+    print('webbrowser installed successfully.')
+
+try:
+    import shutil
+    print('shutil is already available.')
+except ImportError:
+    print('shutil is not available. Installing...')
+    subprocess.check_call(['pip3', 'install', 'shutil'])
+    print('shutil installed successfully.')
